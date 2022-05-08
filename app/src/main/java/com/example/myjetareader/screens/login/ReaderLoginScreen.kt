0f879778f61a1c33186e810ccb3e4817dbb8d221
syogo-user <s.photo.trumpet.005@gmail.com>
2022-05-08 23:21:50 +0900

    パスワード入力欄を作成

--- app/src/main/java/com/example/myjetareader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/myjetareader/screens/login/ReaderLoginScreen.kt
@@ -1,15 +1,15 @@
 package com.example.myjetareader.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -19,18 +19,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.myjetareader.components.EmailInput
+import com.example.myjetareader.components.PasswordInput
 import com.example.myjetareader.components.ReaderLogo
 
 
+@ExperimentalComposeUiApi
 @Composable
 fun ReaderLoginScreen(navController: NavController) {
     Surface(modifier = Modifier.fillMaxSize()) {
@@ -39,6 +44,9 @@ fun ReaderLoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.Center
         ) {
             ReaderLogo()
+            UserForm(loading = false, isCreateAccount = false) { email ,pwd ->
+                Log.d("Form", "ReaderLoginScreen: $email @password")
+            }
         }
     }
 }
@@ -46,7 +54,11 @@ fun ReaderLoginScreen(navController: NavController) {
 @ExperimentalComposeUiApi
 @Preview
 @Composable
-fun UserForm() {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, pwd -> }
+) {
     val email = rememberSaveable { mutableStateOf("") }
     val password = rememberSaveable { mutableStateOf("") }
     val passwordVisibility = rememberSaveable { mutableStateOf(false) }
@@ -65,12 +77,22 @@ fun UserForm() {
     ) {
         EmailInput(
             emailState = email,
-            enabled = true,
+            enabled = !loading,
             onAction = KeyboardActions {
                 passwordFocusRequest.requestFocus()
             }
         )
-
+        PasswordInput(
+            modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enable = !loading,
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(email.value.trim(), password.value.trim())
+            }
+        )
     }
 
 }
