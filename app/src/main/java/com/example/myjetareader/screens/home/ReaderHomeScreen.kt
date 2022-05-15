b8793e77a82a2cb53114aa1bdb13a7c21a2d73cb
syogo-user <s.photo.trumpet.005@gmail.com>
2022-05-15 16:56:23 +0900

    Home画面のTopAppBarを作成

--- app/src/main/java/com/example/myjetareader/screens/home/ReaderHomeScreen.kt
+++ app/src/main/java/com/example/myjetareader/screens/home/ReaderHomeScreen.kt
@@ -1,11 +1,114 @@
 package com.example.myjetareader.screens.home
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.myjetareader.R
+import com.example.myjetareader.navigation.ReaderScreens
+import com.example.myjetareader.screens.login.ReaderLoginScreen
+import com.google.firebase.auth.FirebaseAuth
 
 @Composable
 fun Home(navController: NavController) {
-    Text("I'm home")
+    Scaffold(
+        topBar = {
+                 ReaderAppBar(title = "A.Reader", navController = navController )
+        },
+        floatingActionButton = {
+            FABContent {
 
+            }
+        },
+
+        ) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            // home content
+        }
+    }
+
+}
+
+@Composable
+fun ReaderAppBar(
+    title: String,
+    showProfile: Boolean = true,
+    navController: NavController,
+
+) {
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (showProfile) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "Logo Icon",
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(12.dp))
+                            .scale(0.9f)
+                    )
+                }
+                Text(
+                    text = title,
+                    color = Color.Red.copy(alpha = 0.7f),
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                )
+                Spacer(modifier = Modifier.width(150.dp))
+
+
+            }
+        },
+        actions = {
+                  IconButton(onClick = {
+                      FirebaseAuth.getInstance().signOut().run {
+                          navController.navigate(ReaderScreens.LoginScreen.name)
+                      }
+                  }) {
+                      Icon(imageVector = Icons.Filled.Logout,
+                          contentDescription = "Logout" )
+
+                  }
+        },
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp
+    )
+}
+
+@Composable
+fun FABContent(onTap: () -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap() },
+        shape = RoundedCornerShape(50.dp),
+        backgroundColor = Color(0xFF92CBDF)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add a Book",
+            tint = Color.White
+        )
+
+    }
 }
